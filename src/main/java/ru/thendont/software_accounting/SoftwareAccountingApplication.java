001c87fbbe68cbb44c54f151b784d212c4b67399
thendontuser <koryakinaa.0804@gmail.com>
2025-10-14 18:56:32 +0300

    Исправил сущности, и сделал траничку входа в систему

---
+++ src/main/java/ru/thendont/software_accounting/SoftwareAccountingApplication.java
@@ -0,0 +1,13 @@
+package ru.thendont.software_accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareAccountingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/thendont/software_accounting/SoftwareAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.thendont.software_accounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
